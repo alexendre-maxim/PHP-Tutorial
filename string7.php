c1111ac8ec2f1209bf30635b526a68db52ee4ada
alexendre-maxim <38282564+alexendre-maxim@users.noreply.github.com>
2019-02-06 15:32:22 +0100

    Add files via upload

---
+++ string7.php
@@ -0,0 +1,21 @@
+<?php
+
+	echo "<h3>Abs Method</h3>";
+
+	$numberAbs = -25;
+
+	echo abs($numberAbs);
+
+	echo "<h3>Acos Method</h3>";
+
+	echo acos(0);
+
+	echo "<h3>Acosh Method</h3>";
+
+	echo acosh(3);
+
+	echo "<h3>Asin Method</h3>";
+
+	echo asin(0);
+
+?>
